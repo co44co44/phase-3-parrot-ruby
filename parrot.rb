c54b3933e7edf580be443bece0583c7d0f71e35e
Maria Cristina Ordonez <crisordonez44@gmail.com>
2023-04-02 22:32:08 -0400

    commit tree 6995d766bbcbb4914dc5806cf3cbdc04d58b56b7

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,12 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+require "pry"
+
+def parrot (string="Squawk!")
+    puts string
+    return string
+end
+
+parrot
+
+binding.pry
